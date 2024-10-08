54c94b5e19e0f9d36654029bcddf50943001b462
Yosep Kim <yosepk@gmail.com>
2024-10-08 01:24:59 -0400

    makes the HDB function to work properly

--- hdb/function.js
+++ hdb/function.js
@@ -10,58 +10,72 @@ function generateHashKey(stringInput) {
     return hash;
   }
   
-
-const getAll = async (server, { hdbCore, logger }) => {
-    server.get("/stream", async (request, reply) => {
-        try {
-            const question = request.query.question;
-
-            const hashKey = generateHashKey(JSON.stringify(question));
-            const selectQuery = {
-                body: {
-                    operation: 'search_by_id',
-                    database: 'apicache',
-                    table: 'cache',
-                    ids: [hashKey],
-                    get_attributes: ['id']
-                }
-            };
-
-            const cachedPayload = await hdbCore.requestWithoutAuthentication(selectQuery);
-            if (cachedPayload.length === 1) {
-                delete cachedPayload[0]['id'];
-                delete cachedPayload[0]['__createdtime__'];
-                delete cachedPayload[0]['__updatedtime__'];
-
-                reply.send(cachedPayload[0]);
-            } else {
-                const response = await fetch("https://origin-stream.edgecloud9.com/stream");
-
-                reply.raw.writeHead(200, { 'Content-Type': 'application/stream+json' });
-                for await (const part of response.body.values()) {
-                    reply.raw.write(part);
-                }
-
-                const dbRecord = { 
-                  id: hashKey,
-                  content: response.body
-                }
-                const insertQuery = { 
+  function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
+  
+  const getAll = async (server, { hdbCore, logger }) => {
+      server.get("/stream", async (request, reply) => {
+          try {
+              const question = request.query.question;
+  
+              const hashKey = generateHashKey(JSON.stringify(question));
+              const selectQuery = {
                   body: {
-                    operation: "upsert",
-                    schema: "apicache",
-                    table: "cache",
-                    records: [dbRecord]
+                      operation: 'search_by_id',
+                      database: 'apicache',
+                      table: 'cache',
+                      ids: [hashKey],
+                      get_attributes: ['content']
                   }
-                };
-                hdbCore.requestWithoutAuthentication(insertQuery);
-
-                return reply.raw.end();
-            }
-        } catch (exception) {
-            reply.send({ msg: `miami error occured: ${exception}`, detail: exception.message });
-        }
-    });
-}
-export default getAll;
-
+              };
+  
+              reply.raw.writeHead(200, { 'Content-Type': 'application/stream+json' });
+  
+              const cachedPayload = await hdbCore.requestWithoutAuthentication(selectQuery);
+              if (cachedPayload.length === 1) {
+                  delete cachedPayload[0]['id'];
+                  delete cachedPayload[0]['__createdtime__'];
+                  delete cachedPayload[0]['__updatedtime__'];
+  
+                  const textArray = cachedPayload[0]['content'].split(' ');
+                  for (let i = 0; i < textArray.length; i++) {
+                      reply.raw.write(`${textArray[i]} `);
+                      await sleep(100); // Simulates a delay
+                  }
+                  return reply.raw.end();
+  
+              } else {
+                  const response = await fetch("https://origin-stream.edgecloud9.com/stream");
+                  let textResult = '';
+  
+                  for await (const part of response.body.values()) {
+                      reply.raw.write(part);
+                      textResult += new TextDecoder().decode(part);
+                  }
+  
+                  const dbRecord = { 
+                    id: hashKey,
+                    content: textResult.trim()
+                  }
+                  const insertQuery = { 
+                    body: {
+                      operation: "upsert",
+                      schema: "apicache",
+                      table: "cache",
+                      records: [dbRecord]
+                    }
+                  };
+                  hdbCore.requestWithoutAuthentication(insertQuery);
+  
+                  return reply.raw.end();
+              }
+          } catch (exception) {
+              reply.send({ msg: `miami error occured: ${exception}`, detail: exception.message });
+          }
+      });
+  }
+  export default getAll;
+  
+  
+  
